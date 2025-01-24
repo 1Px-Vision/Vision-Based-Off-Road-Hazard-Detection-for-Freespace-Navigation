70bfbe1b8dd425903d33b489a5f1e98185f0df3c
1Px-Vision <150855410+1Px-Vision@users.noreply.github.com>
2025-01-24 17:51:20 -0600

    Add files via upload

--- Project: System Monitor/src/format.cpp
+++ Project: System Monitor/src/format.cpp
@@ -8,7 +8,7 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { 
+string Format::ElapsedTime(long seconds) { 
     long remainingSeconds = seconds;
     int hours = remainingSeconds / 3600;
     remainingSeconds = remainingSeconds % 3600;
